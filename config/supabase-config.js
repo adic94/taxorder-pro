a05572a8f3bbb6d9feee1cdf404db9bbe33811be
Adam Cichocki <acichocki@mtoilet.local>
2026-06-03 21:56:26 +0200

    Dodano konfiguracje Supabase

---
+++ config/supabase-config.js
@@ -0,0 +1,4 @@
+﻿window.TAXORDER_SUPABASE = {
+  url: "https://opeqckxxdqicszfycolb.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wZXFja3h4ZHFpY3N6Znljb2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDU0MzQsImV4cCI6MjA5NjA4MTQzNH0.FKtWOHc6dFTe9XmsxdI4te_u6MZ7Ac1Hm2PqN8VtnaA"
+};
